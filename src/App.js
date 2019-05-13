e95d2d256311a2ad82d49a6d5d6e740311877c28
w1028eva <w1028eva@gmail.com>
2019-05-13 23:39:34 +0800

    v1
    v1

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import React, { useState } from "react";
+import DateFnsUtils from "@date-io/date-fns"; // import
+import { DatePicker, MuiPickersUtilsProvider ,TimePicker} from "material-ui-pickers";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  const [selectedDate, handleDateChange] = useState(new Date());
+  const style = {
+            padding: '5% 0',    
+    };
+  return (
+  <div class="container" style={style}>
+  <div class="row">  
+   <MuiPickersUtilsProvider utils={DateFnsUtils}>  
+   <div style={style} class="col-lg-6 col-md-5 col-xs-12 text-center">
+      <DatePicker   value={selectedDate} onChange={handleDateChange} />	 
+   </div>	
+   <div class="col-lg-6 col-md-5 col-xs-12 text-center">   
+      <TimePicker value={selectedDate} onChange={handleDateChange} />
+   </div>
+    </MuiPickersUtilsProvider>
+	</div>
+	</div>
+  );
+}
+
+export default App;
